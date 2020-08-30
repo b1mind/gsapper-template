2cc0feceb6cc84ab5483eb49f6eed7e8fbc6ffbd
b1mind <brent@1mindsolutions.com>
2020-08-30 01:31:14 -0600

    🧙‍♂️ component triggered

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,34 @@
+<script>
+  import MyTrigger from '../components/MyTrigger.svelte'
+</script>
+
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
-
+<div class="spacer" />
 <p>This is the 'about' page. There's not much here.</p>
+<div class="spacer" />
+
+<!-- prettier-ignore -->
+<MyTrigger 
+	stOptions={{ pin: true, scrub: false, start: 'center center', end: '300 top' }} 
+	tween={{ autoAlpha: 0, y: 100, stagger: 0.5 }}>
+  <span>one</span>
+  <span>two</span>
+  <span>three</span>
+  <span>four</span>
+  <span>five</span>
+  <span>six</span>
+</MyTrigger>
+<div class="spacer" />
+
+<style lang="scss">
+  .spacer {
+    min-height: 700px;
+    margin: 1rem 0;
+    background: lightblue;
+    text-align: center;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,91 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<script>
+  import { onMount } from 'svelte'
+  import { gsap } from 'gsap'
+  import MyTrigger from '../components/MyTrigger.svelte'
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  let tl = gsap.timeline({})
+  function setTimelines() {
+    tl.to('.pinned', { background: 'blue' }).to('.someText', { color: 'yellow' }).to('.someText', { x: 200 })
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
+  onMount(() => {
+    setTimelines()
+  })
+</script>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>gSapper template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<!-- prettier-ignore -->
+<template lang="pug">
+	h1 this is a pug header
+</template>
 
 <figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
+  <img alt="Success Kid" src="successkid.jpg" />
+  <figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div class="spacer">
+  <div class="test">
+    <MyTrigger stClass="pinned" {tl} stOptions={{ pin: true, start: 'top 200', toggleActions: 'play reverse play reverse' }}>
+      <div class="someText">Some timeline</div>
+    </MyTrigger>
+  </div>
+</div>
+
+<MyTrigger stClass="test" stOptions={{ pin: true, scrub: true, start: 'center center', end: '300 top' }} tween={{ autoAlpha: 0, y: 100, stagger: 0.5 }}>
+  <span>one</span>
+  <span>two</span>
+  <span>three</span>
+  <span>four</span>
+  <span>five</span>
+  <span>six</span>
+</MyTrigger>
+
+<div class="spacer" />
+
+<style lang="scss">
+  h1,
+  figure {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  .test {
+    padding: 10px;
+    background: burlywood;
+  }
+
+  .spacer {
+    min-height: 700px;
+    margin: 1rem 0;
+    background: lightblue;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    min-height: 250px;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
